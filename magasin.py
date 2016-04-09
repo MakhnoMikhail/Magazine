f7fedfa70bc8ee5c99a0ee80c6f9cde9dc0fe052
MakhnoMikhail <mickle1996@mail.ru>
2016-04-09 14:41:39 +0300

    Update magasin.py

--- magasin.py
+++ magasin.py
@@ -1,6 +1,7 @@
 p = 'Нет'
 R = 'Нет'
 k = 0
+flag = 0
 while p != 'Да':
     print ("Выберите раздел:")
     print ("1. Продажа товара")
@@ -9,6 +10,8 @@ while p != 'Да':
     first_number = int(input('Введите число: '))
     my_file = open('склад.txt', 'r+')
     my_string = my_file.readlines()
+    my_sale = open('продажи.txt', 'r+')
+    my_str = my_sale.readlines()
     if (first_number == 1):
         for i in my_string:
             print(i)
@@ -27,20 +30,20 @@ while p != 'Да':
                         my_string[k] = my_string[k].split(',')
                         i[3] = str(int(i[3]) - quantity) + '\n'
                         my_string[k] = i[0] + ',' + i[1] + ',' + i[2] + ',' + i[3]
-                        #my_string += str(i[0]) + ',' + str(int(i[2])*quantity) + ' руб'
+                        my_str += str(i[0]) + ',' + str(int(i[2])*quantity) + ' руб' + '\n'
                         with open('склад.txt', 'w') as my_file:
                             for i in my_string:
                                 my_file.writelines(i)
                         my_file.close()
-                        '''with open ('продажи.txt', 'a') as my_file:
-                            for j in my_string:
-                                my_file.writelines(j)
-                        my_file.close()    '''
+                        with open ('продажи.txt', 'w') as my_sale:
+                            for j in my_str:
+                                my_sale.writelines(j)
+                        my_sale.close()    
                         R = input('Сформировать покупку? ')
+                        if R == 'Да':
+                            print('Спасибо за покупку!\n')
                         break
                     k += 1
-                    if R == 'Да':
-                        break
                 k = 0
                 
             elif (second_number == 2):
@@ -52,24 +55,25 @@ while p != 'Да':
                         my_string[k] = my_string[k].split(',')
                         i[3] = str(int(i[3]) - quantity) + '\n'
                         my_string[k] = i[0] + ',' + i[1] + ',' + i[2] + ',' + i[3]
-                        #my_string += str(i[0]) + ',' + str(int(i[2])*quantity) + ' руб'
+                        my_str += str(i[0]) + ',' + str(int(i[2])*quantity) + ' руб' + '\n'
                         with open('склад.txt', 'w') as my_file:
                             for i in my_string:
                                 my_file.writelines(i)
                         my_file.close()
-                        '''with open ('продажи.txt', 'a') as my_file:
-                            for j in my_string:
-                                my_file.writelines(j)
-                        my_file.close()    '''
+                        with open ('продажи.txt', 'w') as my_sale:
+                            for j in my_str:
+                                my_sale.writelines(j)
+                        my_sale.close()    
                         R = input('Сформировать покупку? ')
+                        if R == 'Да':
+                            print('Спасибо за покупку!\n')
                         break
                     k += 1
-                    if R == 'Да':
-                        break
                 k = 0
             else:
                 R = input('Сформировать покупку? ')
                 if R == 'Да':
+                    print('Спасибо за покупку!\n')
                     break
     if first_number == 2:
         my_file = open('склад.txt', 'r+')
@@ -82,6 +86,7 @@ while p != 'Да':
         for i in my_string:
             i = i.split(',')
             if ident == int(i[0]):
+                flag += 1
                 i[3] = str(int(i[3]) + delivery_number)
                 my_string[k] = i[0] + ',' + i[1] + ',' + i[2] + ',' + i[3] + '\n'
                 with open('склад.txt', 'w') as my_file:
@@ -89,35 +94,20 @@ while p != 'Да':
                         my_file.writelines(i)
                 my_file.close()
             k += 1
-            """else:
-                name = input('Введите название товара: ')
-                price = int(input('Введите цену: '))
-                my_string += '\n' + str(ident) + ',' + str(name) + ',' + str(price) + ',' + str(delivery_number)
-            break"""
-            
-            
-            #my_file = open("sklad.txt", 'a')
-            """content = ['{} {} {} {}'.format(s.split()[1],
-                              s.split()[2], s.split()[3],
-                              s.split()[4]) for s in my_file.read().split('\n')]
-            content.append('{} {}'.format(id, product_name, price, delivery_number))
-            my_file.write("id, product_name, price, delivery_number")
-            my_file = open('склад.txt', 'r')
-            my_string = my_file.read()
-            print(my_string)
-            my_file.close()"""
+        if flag == 0:
+            name = input('Введите название товара: ')
+            price = int(input('Введите цену: '))
+            with open('склад.txt', 'w') as my_file:
+                my_string += str(ident) + ',' + str(name) + ',' + str(price) + ',' + str(delivery_number) + '\n'
+                for i in my_string:
+                    my_file.writelines(i)
+            my_file.close()
+        flag = 0    
         k = 0    
     if (first_number == 3):
-        
-       ''' with open('продажи.txt', 'w') as my_file:
-            for i in my_string:
-                my_file.writelines(i)
-        my_file.close()
-        my_file = open("продажи.txt", "w")
-        my_string = my_file.writelines()
-        my_file = open("продажи.txt", "r")
-        my_string = my_file.read()
-        print(my_string)
-        my_file.close()'''
+        my_sale = open("продажи.txt", "r+")
+        my_str = my_sale.read()
+        print(my_str)
+        my_sale.close()
     R = 'Нет'    
     p = input('Все? ')
